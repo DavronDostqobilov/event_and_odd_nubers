c2f191323db90b94959731c261ff050302945f72
DavronDostqobilov <davrondostqobilov885@gmail.com>
2022-11-14 19:25:05 +0300

    is_odd

--- is_odd.py
+++ is_odd.py
@@ -1,3 +1,8 @@
 #Create a variable "number" and assign it a integer value.
-
+def main(number):
+    if number%2==0:
+        return 0
+    elif number%2!=0:
+        return 1  
 #Print 1 if the number is odd, otherwise print 0.
+print(main(13))
