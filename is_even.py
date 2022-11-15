724f304ebfec92aec88fe3d574d5b46abcc162b1
DavronDostqobilov <davrondostqobilov885@gmail.com>
2022-11-15 16:34:20 +0300

    is_even

--- is_even.py
+++ is_even.py
@@ -1,8 +1,4 @@
 #Create a variable "number" and assign it a integer value.
-def main(number):
-    if number%2==0:
-        return 1
-    elif number%2!=0:
-        return 0   
+number=25  
 #Print 1 if the number is even, otherwise print 0.
-print(main(12))
+print((number+1)%2)
